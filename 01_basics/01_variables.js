436d6a14c3ba0698874abafaefefd75768eedd9f
SAMZ-01 <alimustafazaidiaha@gmail.com>
2024-08-25 17:17:58 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144567
+let accountEmail = "hchchchc@.com"
+var accountPassword = "1222223"
+acountCity = "karachi"
+let accountState;
+
+// accountId = 2 // never do this
+
+accountEmail = "asdfghj"
+accountPassword = "135475858"
+accountCity = "lahore"
+
+console.log(accountId);
+
+/* prefer not to use var instead use let
+ due to block scope and functional scope
+ */
+
+ console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
