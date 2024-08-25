940a7ad5666f6d144198b37e67aa8110bede86c3
SAMZ-01 <alimustafazaidiaha@gmail.com>
2024-08-25 18:05:43 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "ali"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof (valueInNumber))
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let loggedIn = 'ali'
+//console.log(typeof loggedIn);
+
+let boolienLoggedIn = Boolean(loggedIn)
+
+//console.log(typeof boolienLoggedIn);
+//console.log(boolienLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+console.log(typeof someNumber)
+
+let stringNumber = String(someNumber)
+//console.log(typeof stringNumber);
+//console.log(stringNumber);
+
+
